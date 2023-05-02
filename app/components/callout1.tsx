f6800c364b278ba577b75a13571e10ec2796673a
evolshenanigans <gjgutierrez91@gmail.com>
2023-05-01 19:48:49 -0700

    modified:   app/components/callout1.tsx

--- app/components/callout1.tsx
+++ app/components/callout1.tsx
@@ -11,6 +11,8 @@ const Callout1: FunctionComponent = () => {
           src={bgimg}
           alt="Background"
           className="object-fit object-center h-screen w-screen"
+          width={1920}
+            height={1080}
         />
       </div>
       <h1 className="font-serif font-semibold text-7xl drop-shadow-md z-10">
@@ -18,8 +20,8 @@ const Callout1: FunctionComponent = () => {
       </h1>
       <p className="mx-auto font-serif text-lg w-3/4 z-10">
         Car crashes are the No. 1 killer of teenagers. Vehicle safety
-        technologies are part of the solution. Learn more about how to help
-        teens stay safe on the road
+        technologies are part of the solution. Learn more about how to help teens
+        stay safe on the road
       </p>
     </div>
   );
