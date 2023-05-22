4b759d120c7e0fc5a6f59ee5b0655b4c59e9650c
Josh <gjgutierrez91@gmail.com>
2023-05-22 13:36:48 -0700

    modified:   app/components/footer.tsx 	new file:   app/components/images/testimonials1.png 	new file:   app/components/images/testimonials2.png

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,38 +1,60 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import testimg1 from "../components/images/testimonials1.png";
+import testimg2 from "../components/images/testimonials2.png";
 //create 2 divs one on top of eachother the top div should have a background color of #e09900 and the bottom one should have a background color of #59406c
 function Footer() {
-    return (
-      <footer className="flex flex-col w-full bg-[#e5e5e5]">
-        <div className="flex flex-row justify-between items-center text-center w-full bg-[#e09900] rounded-t-2xl">
-          <div className="flex flex-col justify-center text-center w-1/3 h-64 px-4">
-            <h1 className="font-extrabold text-[#333333] text-xl">Contact us</h1>
-            <p className="text-gray-800 mt-4">1050 Lakes Drive West<br />Covina, CA 91790</p>
-            <p className="text-gray-800 mt-4">Phone: 555-1234<br />info@featuresfirstauto.com</p>
-          </div>
-          <div className="flex flex-col justify-center text-center w-1/3 h-64 px-4">
-            <h1 className="font-extrabold text-[#333333] text-xl">Quick Links</h1>
-            <ul className="mt-4 ">
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Our Podcast</a></li>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Our Solutions</a></li>
-            </ul>
-          </div>
-          <div className="flex flex-col justify-center text-center w-1/3 h-64 px-4">
-            <h1 className="font-extrabold text-[#333333] text-xl">Subscribe to Our Newsletter</h1>
-            <form className="mt-4">
-              <input type="email" placeholder="Email" className="mt-2 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#4f4f4f] focus:border-transparent" />
-              <button type="submit" className="mt-4 bg-[#D94F33] hover:bg-[#59406C] text-white font-bold py-2 px-4 rounded">Submit</button>
-            </form>
-          </div>
+  return (
+    <footer className="flex flex-col w-full bg-[#e5e5e5]">
+      <div className="card card-side bg-base-100 shadow-xl relative">
+        <figure className="relative w-full h-[300px]">
+          <Image src={testimg1} alt="Movie" />
+        </figure>
+        <div className="card-body">
+          <p>
+            "Being a healthcare professional that is focused on education and
+            risk reduction I was able to clearly see how this product could
+            benefit companies that are advocating for vehicle health and driver
+            safety (a yin yang). There is a clear position in the market for
+            FeaturesFirst Auto's' mission and vision. They are driven by a
+            fierce leader who is very capable of efficiently executing this
+            vision.""
+          </p>
+          <h2 className="card-title">
+            Hayley Murphy - Jaguar Range Rover Consumer, RN
+          </h2>
         </div>
-        <div className="flex flex-col w-full bg-[#59406c] h-10 p-5">
-          <h1 className="text-white text-sm text-center">
-            ©2019 FeaturesFirst Auto, a multipurpose automotive platform. | Privacy Policy | Terms &amp; Conditions | Disclaimer
-          </h1>
+      </div>
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure>
+          <Image src={testimg2} alt="Movie" />
+        </figure>
+        <div className="card-body">
+          <p>
+            "FeaturesFirst Auto from a business perspective reaches EVERYONE
+            with a car and EVERYONE that knows somebody with a car (i.e.
+            everyone). The cars we drive can inspire different energies within
+            us, and we need to trust and rely on our cars to get us, our
+            passengers, and other drivers on the road to where we're going,
+            SAFELY. Safety recalls are so common across all manufacturers,
+            makes, and models- and the consumer hardly knows how to do anything
+            about it! FeaturesFirst Auto keeps drivers, and their loved ones
+            SAFE and CONFIDENT on the road.""
+          </p>
+          <h2 className="card-title">
+            Brendan Polsin - Chevrolet Impala Consumer, Pharmaceutical
+            Representative
+          </h2>
         </div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+      </div>
+      <div className="flex flex-col w-full bg-[#59406c] h-10 p-5">
+        <h1 className="text-white text-sm text-center">
+          ©2019 FeaturesFirst Auto, a multipurpose automotive platform. |
+          Privacy Policy | Terms &amp; Conditions | Disclaimer
+        </h1>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
