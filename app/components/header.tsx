0426f7519d2325ec9c863acae69467f4a7d2d100
evolshenanigans <gjgutierrez91@gmail.com>
2023-05-22 23:27:33 -0700

    fixed bug in header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,7 +8,7 @@ function header() {
         <Image src={Headerpic} alt="" className="h-full w-full rounded-xl" />
         <div className="absolute top-1/3 left-[20%] transform -translate-x-1/2 -translate-y-1/2 w-96 flex flex-col items-center justify-center">
           <h1 className="font-extrabold text-left text-[#59406c] mb-0">
-            Driver's education relevant to today's vehicles
+            Driver&apos;s education relevant to today&apos;s vehicles
           </h1>
           <p className="font-serif text-center">greet the road confidently with the knowledge and skills needed to interact effectively with new safety vehicle technologies.</p>
           <button className="bg-[#fd9862] text-[#59406c] font-bold py-2 px-4 rounded-md shadow-md">
