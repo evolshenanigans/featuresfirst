082c97887084a2743eb768e0e0392e177029d68f
Josh <gjgutierrez91@gmail.com>
2023-05-23 14:03:09 -0700

    fixed mission statement

--- app/components/missionStatement.tsx
+++ app/components/missionStatement.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import Image from 'next/image';
-import MissionStatement from '../components/images/safetyauto.jpg';
+import React from "react";
+import Image from "next/image";
+import MissionStatement from "../components/images/safetyauto.jpg";
 
 function missionStatement() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="relative h-[300px] w-[300px]">
-        <Image src={MissionStatement} alt="missionstatement" layout="fill" objectFit="cover" />
-      </div>
-      <div className="ml-4">
-        <h1>Our Sustainability Mission Statement</h1>
-        <p>
-          We are dedicated to environmental improvements that foster a sustainable future and lead to social and
-          economical improvements in the communities we do business.
-        </p>
+    <div className="card card-side bg-white shadow-2xl m-3">
+      <figure>
+        <Image
+          src={MissionStatement}
+          alt="missionstatement"
+          className="rounded-box m-1 "
+        />
+      </figure>
+      <div className="card-body text-black">
+        <h2 className="card-title">Our Sustainability Mission Statement</h2>
+        <p> We are dedicated to environmental improvements that foster a sustainable future and lead to social and economical improvements in the communities we do business.</p>
       </div>
     </div>
   );
