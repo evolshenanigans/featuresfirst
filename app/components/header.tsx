80898d4ed152265ce4249b670094052119db116e
Josh <gjgutierrez91@gmail.com>
2023-06-05 13:40:25 -0700

    header title changed

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,20 +3,20 @@ import Headerpic from "../components/images/featuresfirstheader.png";
 import Image from "next/image";
 
 function header() {
-    return (
-      <div className="bg-[#e5e5e5] h-screen w-screen overflow-hidden">
-        <Image src={Headerpic} alt="" className="h-full w-full rounded-xl" />
-        <div className="absolute top-1/3 left-[20%] transform -translate-x-1/2 -translate-y-1/2 w-96 flex flex-col items-center justify-center">
-          <h1 className="font-extrabold text-left text-[#59406c] mb-0">
-            Driver&apos;s education relevant to today&apos;s vehicles
-          </h1>
-          <p className="font-serif text-center">greet the road confidently with the knowledge and skills needed to interact effectively with new safety vehicle technologies.</p>
-          <button className="bg-[#fd9862] text-[#59406c] font-bold py-2 px-4 rounded-md shadow-md">
-            Learn More
-          </button>
-        </div>
+  return (
+    <div className="bg-[#e5e5e5] h-screen w-screen overflow-hidden">
+      <Image src={Headerpic} alt="" className="h-full w-full rounded-xl" />
+      <div className="absolute top-1/3 left-[20%] transform -translate-x-1/2 -translate-y-1/2 w-96 flex flex-col items-center justify-center">
+        <h1 className="font-extrabold text-center text-[#59406c] mb-0">
+          Drivers education relevant to today's vehicles for drivers, passengers
+          and everyone else on the road
+        </h1>
+        <button className="bg-[#fd9862] text-[#59406c] font-bold py-2 px-4 rounded-md shadow-md">
+          Learn More
+        </button>
       </div>
-    );
-  }
-  
-  export default header;
+    </div>
+  );
+}
+
+export default header;
