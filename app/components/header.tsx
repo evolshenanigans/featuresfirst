7b47065bdd7efb0f69a372a78680a1108a8e9cb4
Josh <gjgutierrez91@gmail.com>
2023-06-11 13:47:47 -0700

    fixed header font

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 function header() {
   return (
-    <div className="bg-[#e5e5e5] h-screen w-screen overflow-hidden">
+    <div className="bg-[#e5e5e5] h-screen w-screen overflow-hidden relative">
       <Image src={Headerpic} alt="" className="h-full w-full rounded-xl" />
-      <div className="absolute top-1/3 left-[20%] transform -translate-x-1/2 -translate-y-1/2 w-96 flex flex-col items-center justify-center">
-        <h1 className="font-manrope font-light text-2xl text-center text-black mb-0">
+      <div className="absolute top-20 left-5 lg:top-20 lg:left-10 max-w-sm md:max-w-md lg:max-w-lg flex flex-col items-start justify-center space-y-4 p-4">
+        <h1 className="font-manrope font-normal text-2xl text-start text-black mb-0">
           Drivers education relevant to today&apos;s vehicles for drivers,
           passengers and everyone else on the road
         </h1>
@@ -18,5 +18,7 @@ function header() {
     </div>
   );
 }
+
+
 
 export default header;
