c36c9d71a366e3bcabb55287b7829ec83d081bd4
evolshenanigans <gjgutierrez91@gmail.com>
2023-06-21 16:31:44 -0700

    modified:   app/components/carousel.tsx

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -7,44 +7,6 @@ import blogimg from "../components/images/Featurestalkblog.jpg";
 
 function carousel() {
   return (
-<<<<<<< HEAD
-    <div className="carousel carousel-center p-4 space-x-4 rounded-box w-full">
-      <div id="slide1" className="carousel-item relative">
-        <Image src={adas} className="rounded-box" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative">
-        <Image src={podcastimg} className="rounded-box" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative">
-        <Image src={blogimg} className="rounded-box" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-=======
 //     <div className="flex justify-center">
 //       <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
 //         <div className="carousel-item">
@@ -60,7 +22,6 @@ function carousel() {
 //     </div>
  <div></div>
  );
->>>>>>> 43753e8c5bf969185940bfd4db9b5b114c650f85
 }
 
 export default carousel;
